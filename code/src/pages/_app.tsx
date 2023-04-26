c205151ebaee011ebb480e0d1fd2b82bc85a0651
Kuba Pawlak <kubapawlak1051@wp.pl>
2023-04-26 22:23:43 +0200

    Created Next app with password-utils file

---
+++ code/src/pages/_app.tsx
@@ -0,0 +1,22 @@
+import { Fragment } from "react";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
+import "@/styles/globals.css";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Fragment>
+      <Head>
+        <title>Password Generator</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="This is a simple web application built using Next.js, TypeScript and Material UI. The app generates random passwords based on user preferences and specifications."
+        />
+      </Head>
+      <Component {...pageProps} />
+    </Fragment>
+  );
+}
